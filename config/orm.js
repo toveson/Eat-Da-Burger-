bd9d4ce34fff538a9ff0cb5b4c225f0b06280aed
Trever O <toveson9522@gmail.com>
2020-11-09 20:51:37 -0700

    built sql queries to select all, insert one, and update one

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,37 @@
-// 3. Create an `orm.js` file inside `config` directory.
-
 //    * Import (require) `connection.js` into `orm.js`
+const connection = require('../config/connection');
 
 //    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+const orm = {
 
-//      * `selectAll()`
-//      * `insertOne()`
-//      * `updateOne()`
+    //      * `selectAll()`
+    selectAll: async function selectAllBurgers() {
+        return new Promise((res, rej) => {
+            connection.query('SELECT * FROM burgers', function (err, results, fields) {
+                if (err) throw err;
+                res(results);
+            });
+        });
+    },
+    //      * `insertOne()`
+    insertOne: async function insertBurger() {
+        return new Promise((res, rej) => {
+            connection.query('INSERT INTO burgers SET ?', { insertBurger: burger_name, devoured: false }, function (err, results) {
+                if (err) throw err;
+                res(results);
+            });
+        });
+    },
+    //      * `updateOne()`
+    updateOne: async function updateOne() {
+        return new Promise((res, rej) => {
+            connection.query('UPDATE burgers SET ? WHERE ?', [{ devoured: true }, { id: burgerId }], function (err, results) {
+                if (err) throw err;
+                res(results);
+            });
+        });
+    }
+};
 
 //    * Export the ORM object in `module.exports`.
+module.exports = orm;
