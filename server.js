7a91359cda7ce5b64c9e58ac92b20af8590a9943
Trever O <toveson9522@gmail.com>
2020-11-13 22:00:43 -0700

    made server and handlebars layouts

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 // required packages
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+// global vars 
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// setting the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// route for handlebars
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use('/', require('./controllers/burgers_controller'));
+
+// server listener
+app.listen(PORT, () => {
+    console.log('app running on PORT: ' + PORT);
+});
