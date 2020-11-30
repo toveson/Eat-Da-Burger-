aa9f19a3e0534169dc44f158cbf2e76dac2a3f33
Trever O <toveson9522@gmail.com>
2020-11-29 21:55:29 -0700

    conection testing

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,15 @@ const { promisify } = require('util');
 
 
 // if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
 // } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: 'Clear2020',
-//     database: 'burgers_db'
-//   });
+  connection = mysql.createConnection({
+    host: 'r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'otkf86w0h073oy09',
+    password: 'w00cacvl6v6u8la3',
+    database: 'zy3y0dlby3ovk8zf'
+  });
 // };
 
 // Making a connection.
