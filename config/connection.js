51a52674a8815a24fd55281577cd0619b1cfccc3
Trever O <toveson9522@gmail.com>
2020-11-29 22:01:45 -0700

    connection testing

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,17 @@ const mysql = require('mysql');
 const { promisify } = require('util');
 
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
-    host: 'r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'localhost',
     port: 3306,
-    user: 'otkf86w0h073oy09',
-    password: 'w00cacvl6v6u8la3',
-    database: 'zy3y0dlby3ovk8zf'
+    user: 'root',
+    password: 'Clear2020',
+    database: 'burgers_db'
   });
-// };
+}
 
 // Making a connection.
 connection.connect(function (error) {
