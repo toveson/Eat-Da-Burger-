84cd6aac44188bce5775f57a9d374c41e35fe4e9
Trever O <toveson9522@gmail.com>
2020-11-29 21:45:15 -0700

    changed err to error

--- config/connection.js
+++ config/connection.js
@@ -9,18 +9,18 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'Clear2020',
-    database: 'burgers_db'
+    user: 'otkf86w0h073oy09',
+    password: 'w00cacvl6v6u8la3',
+    database: 'zy3y0dlby3ovk8zf'
   });
 };
 
 // Making a connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
+connection.connect(function (error) {
+  if (error) {
+    console.error("error connecting: " + error.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
